e50b052c1a56e23aa55c9c00cde2aa7b06cebca1
allecosta <alle_costa10@outlook.com>
2024-06-11 17:38:02 -0300

    feat: release v0.9.3

--- br/com/library/PublishCompany.java
+++ br/com/library/PublishCompany.java
@@ -4,4 +4,27 @@ public class PublishCompany {
 
 	private String fantasyName, socialReason, cnpj;
 
+	public String getFantasyName() {
+		return fantasyName;
+	}
+
+	public void setFantasyName(String fantasyName) {
+		this.fantasyName = fantasyName;
+	}
+
+	public String getSocialReason() {
+		return socialReason;
+	}
+
+	public void setSocialReason(String socialReason) {
+		this.socialReason = socialReason;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
 }
